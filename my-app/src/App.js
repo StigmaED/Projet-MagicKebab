4bb594c55a5ab8dd7416c4e178cea0fae73b5190
StigmaED <elie.dechasse@hotmail.com>
2021-10-26 15:22:41 +0200

    sd

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,55 @@
-import logo from './logo.svg';
-import fakeData from './ingredient.json';
+//import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
-import React from "react"
+import { useState } from 'react';
+
+function KebabForm(props) { // composant d'un kébab
+    const [pain, setPain] = useState("")
+    const [viande, setViande] = useState("") 
+    const [sto, setSto] = useState("") //Salade tomate  oignons
+    const [viande, setViande] = useState("")
+    const { onSubmit } = props
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    return (
+        <div className="App">
+
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <div className="container">
+                    <p>
+                        Pain ou galette ?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+                    <button>
+                        <div className="Ingredient-card">
+                            <img src="https://i.imgur.com/C0YORKY.jpeg"
+                                alt=" Pain "
+                            />
+
+                            <p>Pain</p>
+
+
+                        </div>
+                    </button>
+
+                    <button>
+                        <div className="Ingredient-card">
+                            <img src="https://i.imgur.com/KOQEGt7.jpeg"
+                                alt=" Galette "
+                            />
+                            <p>Galette</p>
+
+
+                        </div>
+
+                    </button>
+
+                </div>
+
+            </header>
+        </div>
+    );
 }
 
 export default App;
